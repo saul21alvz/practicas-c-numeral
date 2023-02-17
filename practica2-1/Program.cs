283c3a57e5be7329b3cc325f172a828c8931768d
Your Name <you@example.com>
2023-02-17 12:43:34 -0600

    pra2.2

---
+++ practica2-1/Program.cs
@@ -0,0 +1,58 @@
+﻿// See https://aka.ms/new-console-template for more information
+/* hacer un programa que lea nombre de un producto, cantida y precio 
+respectivamente validados y almacenarlos en un archivo, este proceso se realizara hasta que el usuario lo desee
+al finalizar se agregara al archivo el total de productos y el total de los precios, considerando la cantidad de productos */
+
+
+using System;
+
+StreamWriter fichero;
+string nombre = "Venta.txt";
+string dato = "";
+string nom,num,nu,n,bo="s";
+int ca=0, pre=0, sum=0, c=0,cp=0;
+bool t=false, p=false;
+
+while (bo=="s" || bo=="si") {
+    while (t == false) {
+        Console.Write("Escribe el nombre del producto ");
+        nom = Console.ReadLine();
+        Console.Write("Escrive el precio: ");
+        num = Console.ReadLine();
+        Console.Write("Escribe la cantidad del producto ");
+        nu = Console.ReadLine();
+        if (int.TryParse(num, out pre)) {
+            if (int.TryParse(nu, out ca)) {
+                t = true;
+            }
+            else {
+                Console.WriteLine("No son numeros enteros");
+                t = false;
+                c++;
+                cp += ca;
+            }
+        }
+        else {
+            Console.WriteLine("No son numeros enteros");
+            t = false;
+        }
+        sum += pre * ca;
+    }
+    Console.WriteLine("Quieres agregar otro  'si' o 'no'");
+    bo = Console.ReadLine();
+}
+
+try
+{
+    fichero = File.AppendText(nombre);//crea el archivo o lo reutiliza
+    dato = "Total de Productos "+c.ToString()+" \n" +
+        "Total del precio "+sum.ToString()+"\n" +
+        "Cantidad de productos "+cp.ToString();
+    Console.WriteLine(dato);
+    fichero.WriteLine(dato);
+    fichero.Close();//cierra el archivo
+}
+catch (Exception msg) // Si no se puede leer mostramos el error
+{
+    Console.WriteLine("No se encuentra el archivo " + msg);
+}
---
+++ practica2.2/Program.cs
@@ -0,0 +1,27 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+
+using System.Text;
+
+Console.Write("Escribe una cadena ");
+string a = Console.ReadLine();
+byte[] arr = Encoding.ASCII.GetBytes(a);
+int ce = 0,cl=0,cn=0;
+foreach(byte b in arr)
+{
+    //Console.WriteLine(" {0} = {1}", b, (char)b);
+    if ((char)b == 32)//contador espacio
+    {
+        ce++;
+    }
+    if ((char)b >= 49 && (char)b <=57)//contador numeros
+    {
+        cn++;
+    }
+    if ((char)b >= 65 && (char)b <= 90 || (char)b >= 97 && (char)b <= 122)//contador letras
+    {
+        cl++;
+    }
+}
+
+Console.WriteLine("cantidad de espacios {0}, cantidad de numeros {1}, cantidad de letras {2}",ce,cn,cl);
