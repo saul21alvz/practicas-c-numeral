2ff63cf3915d517f79f1b73492634363eafee787
Your Name <you@example.com>
2023-02-27 23:18:56 -0600

    ven-1

--- ventanas2023/ventanas2023/Form1.Designer.cs
+++ ventanas2023/ventanas2023/Form1.Designer.cs
@@ -28,13 +28,119 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.Btnaceptar = new System.Windows.Forms.Button();
+            this.Btnsalir = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.Txtnombre = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtedad = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtsexo = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // Btnaceptar
+            // 
+            this.Btnaceptar.Location = new System.Drawing.Point(174, 424);
+            this.Btnaceptar.Margin = new System.Windows.Forms.Padding(5);
+            this.Btnaceptar.Name = "Btnaceptar";
+            this.Btnaceptar.Size = new System.Drawing.Size(131, 42);
+            this.Btnaceptar.TabIndex = 0;
+            this.Btnaceptar.Text = "Aceptar";
+            this.Btnaceptar.UseVisualStyleBackColor = true;
+            this.Btnaceptar.Click += new System.EventHandler(this.Aceptar_Click);
+            // 
+            // Btnsalir
+            // 
+            this.Btnsalir.Location = new System.Drawing.Point(327, 424);
+            this.Btnsalir.Margin = new System.Windows.Forms.Padding(5);
+            this.Btnsalir.Name = "Btnsalir";
+            this.Btnsalir.Size = new System.Drawing.Size(131, 42);
+            this.Btnsalir.TabIndex = 1;
+            this.Btnsalir.Text = "Salir";
+            this.Btnsalir.UseVisualStyleBackColor = true;
+            this.Btnsalir.Click += new System.EventHandler(this.Btnsalir_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(69, 22);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(101, 29);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Nombre";
+            // 
+            // Txtnombre
+            // 
+            this.Txtnombre.Location = new System.Drawing.Point(74, 59);
+            this.Txtnombre.Name = "Txtnombre";
+            this.Txtnombre.Size = new System.Drawing.Size(300, 34);
+            this.Txtnombre.TabIndex = 3;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(74, 96);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(70, 29);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Edad";
+            // 
+            // txtedad
+            // 
+            this.txtedad.Location = new System.Drawing.Point(74, 128);
+            this.txtedad.Name = "txtedad";
+            this.txtedad.Size = new System.Drawing.Size(300, 34);
+            this.txtedad.TabIndex = 5;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(79, 169);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(331, 29);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Sexo \"hombre = h, mujer = m\"";
+            // 
+            // txtsexo
+            // 
+            this.txtsexo.Location = new System.Drawing.Point(74, 202);
+            this.txtsexo.Name = "txtsexo";
+            this.txtsexo.Size = new System.Drawing.Size(300, 34);
+            this.txtsexo.TabIndex = 7;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(14F, 29F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.BackColor = System.Drawing.SystemColors.Highlight;
+            this.ClientSize = new System.Drawing.Size(686, 515);
+            this.Controls.Add(this.txtsexo);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.txtedad);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.Txtnombre);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.Btnsalir);
+            this.Controls.Add(this.Btnaceptar);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Margin = new System.Windows.Forms.Padding(5);
+            this.Name = "Form1";
+            this.Text = "practica1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button Btnaceptar;
+        private System.Windows.Forms.Button Btnsalir;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox Txtnombre;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtedad;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtsexo;
     }
 }
 
--- ventanas2023/ventanas2023/Form1.cs
+++ ventanas2023/ventanas2023/Form1.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -12,9 +13,92 @@ namespace ventanas2023
 {
     public partial class Form1 : Form
     {
+        List<List<string>> listas = new List<List<string>>();
         public Form1()
         {
             InitializeComponent();
+            
+        }
+        string nombre, edad, sexo;
+
+        private void Aceptar_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                nombre = Txtnombre.Text;
+                edad = txtedad.Text;
+                sexo = txtsexo.Text;
+                if (string.IsNullOrEmpty(nombre) || string.IsNullOrEmpty(edad) || string.IsNullOrEmpty(sexo) )
+                {
+                    MessageBox.Show("Alguna de las casillas esta vacia");
+                }
+                else
+                {
+                    int ce = 0;
+                    int cl = 0;
+                    byte[] arr = Encoding.ASCII.GetBytes(nombre);
+                    foreach (byte b in arr)
+                    {
+                        if ((char)b == 32)//contador espacio
+                        {
+                            ce++;
+                        }
+                        if ((char)b >= 65 && (char)b <= 90 || (char)b >= 97 && (char)b <= 122)//contador letras
+                        {
+                            cl++;
+                        }
+                    }
+                    if (nombre.Length == ce + cl)
+                    {
+                        //siguiente validacion
+                        int f;
+                        if (int.TryParse(edad, out f))
+                        {
+                            //validar sexo
+                            if (sexo == "h" || sexo == "m")
+                            {
+                                List<string> lista = new List<string>();
+                                lista.Add(nombre);
+                                lista.Add(edad);
+                                lista.Add(sexo);
+                                listas.Add(lista);
+                                foreach (var i in listas)
+                                {
+                                    MessageBox.Show(string.Join(", ", i));
+                                }                              
+                            }
+                            else
+                            {
+                                MessageBox.Show("El valor del sexo no es valido");
+                            }
+                        }
+                        else
+                        {
+                            MessageBox.Show("La edad tiene un valor no valido");
+                        }
+                    }
+                    else
+                    {
+                        //mostrar que esta mal el nombre
+                        MessageBox.Show("El nombre tiene valores no validos");
+                    }
+                }
+            }catch(Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+            //lista.add(Txtnombre.ToString());
+            //MessageBox.Show("hola mundo");
+            //MessageBox.Show("otro mensaje");
+        }
+
+        private void Btnsalir_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
         }
     }
 }
+    
+
+
+
